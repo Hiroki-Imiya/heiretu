65b7a50464f393ca43e61669ba11e6223cb4de91
Hiroki-Imiya <yo4ro6si4ku9na7@gmail.com>
2024-05-16 23:57:29 +0900

    並列化のための変数の宣言

--- kadai04A/kadai04A-3.c
+++ kadai04A/kadai04A-3.c
@@ -2,28 +2,37 @@
 //画像を生成するためのライブラリ
 #include<ppexp.h>
 
+//使用するプロセス数
+#define PNUM 1
+
 //画像の横幅
 #define WID 1024
 //画像の縦幅
 #define HEI 1024
-//nの最大値
+//繰り返しの最大回数
 #define U 8000
 
+//1つのプロセスが計算する行数
+#define HEI_PNUM (HEI/PNUM)
+
 //実部の最小値
-#define MIN_A -5.0
+#define MIN_A 1.36769
 //実部の最大値
-#define MAX_A 5.0
+#define MAX_A 1.36789
 //実部の振れ幅
 #define RANGE_A (MAX_A-MIN_A)
 //虚部の最小値
-#define MIN_B -8.0
+#define MIN_B 0.00736
 //虚部の最大値
-#define MAX_B 8.0
+#define MAX_B 0.00756
 //虚部の振れ幅
 #define RANGE_B (MAX_B-MIN_B)
 
 //並列用の関数
 void f(void){
+
+    //自身のプロセス番号を取得
+    int myrank = T_GetMyNum();
 
     //グレー画像情報
     uchar g[HEI][WID];
@@ -32,25 +41,22 @@ void f(void){
     int y,x;
 
     //Mandelbrot集合の描画
-    for(y=0;y<HEI;y++){
+    for(y=myrank*HEI_PNUM;y<(myrank+1)*HEI_PNUM;y++){
         for(x=0;x<WID;x++){
-
             //実部
             double a = MIN_A + RANGE_A*(double)x/(double)WID;
             //虚部
             double b = MIN_B + RANGE_B*(double)y/(double)HEI;
-
             //zの実部
             double zr = 0.0;
             //zの虚部
             double zi = 0.0;
             //zの絶対値の二乗
             double z = 0.0;
-
             //繰り返し回数
             int n = 0;
             
-            //zの絶対値の２乗が4より小さいかつnが最大値より小さい間繰り返す
+            //zの絶対値の２乗が4より小さいかつ繰り返し回数が最大繰り返し回数より小さい間繰り返す
             while(z<4.0 && n<U){
 
                 //次のzの実部を計算
@@ -82,6 +88,6 @@ void f(void){
 
 int main(void){
     //T_Runで並列呼び出し
-    T_Run(1,f); 
+    T_Run(PNUM,f); 
     return 0;
 }
